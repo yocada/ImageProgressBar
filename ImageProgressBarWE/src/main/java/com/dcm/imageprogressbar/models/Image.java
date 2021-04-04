5f873da9ad6722bc4180f1c69da66ca713ae1db8
PC-Carmen\Carmen <dcortesmut@gmail.com>
2021-04-04 11:49:31 +0200

    Ajustes

--- ImageProgressBarWE/src/main/java/com/dcm/imageprogressbar/models/Image.java
+++ ImageProgressBarWE/src/main/java/com/dcm/imageprogressbar/models/Image.java
@@ -1,2 +1,25 @@
-package com.dcm.imageprogressbar.models;public class Image {
+package com.dcm.imageprogressbar.models;
+
+/**
+ * Representa una imagen
+ */
+public class Image {
+
+    private byte[] image;
+
+    public Image(){
+
+    }
+
+    public Image(byte[] image){
+        this.image = image;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
 }
--- ImageProgressBarWE/src/main/java/com/dcm/imageprogressbar/rest/ImageProgressBarREST.java
+++ ImageProgressBarWE/src/main/java/com/dcm/imageprogressbar/rest/ImageProgressBarREST.java
@@ -8,13 +8,12 @@ import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
+import com.dcm.imageprogressbar.models.Image;
 import org.apache.commons.io.IOUtils;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dcm.imageprogressbar.ImageProgressBar;
@@ -32,58 +31,21 @@ public class ImageProgressBarREST {
 	//@GetMapping(value = "download", produces = MediaType.IMAGE_PNG_VALUE)
 	
 	@CrossOrigin(origins = "*")
-	@GetMapping(value = "download")
-	public ResponseEntity<ImgTest> getImageWithMediaType() throws IOException {
-	//public @ResponseBody byte[] getImageWithMediaType() throws IOException {
-		//TEST
-		//Comprobación de parámetros
-//		if (args.length != 3) {
-//			System.out.println("Uso: Ancho Alto Porcentaje");
-//			System.exit(0);
-//		}
-//		int ancho = 0; int alto = 0; float porcentaje = 0;
-//		try {
-//			ancho = Integer.parseInt(args[0]);
-//		}catch(NumberFormatException ex) {
-//			System.out.println("Debe indicar un valor entero para el ancho de la imagen");
-//			System.exit(0);
-//		}
-//		try {
-//			alto = Integer.parseInt(args[1]);
-//		}catch(NumberFormatException ex) {
-//			System.out.println("Debe indicar un valor entero para el alto de la imagen");
-//			System.exit(0);
-//		}
-//		try {
-//			porcentaje = Float.parseFloat(args[2]);
-//		}catch(NumberFormatException ex) {
-//			System.out.println("Debe indicar un valor decimal para el porcentaje de progreso");
-//			System.exit(0);
-//		}catch(NullPointerException ex) {
-//			System.out.println("Debe indicar un valor decimal para el porcentaje de progreso");
-//			System.exit(0);
-//		}
-		
+	@GetMapping(value = "get")
+	public ResponseEntity<Image> getImage() throws IOException {
+
 		ImageProgressBar bProgreso = new ImageProgressBar();
 		bProgreso.setColors(ImageProgressBar.Color.BLANCO, ImageProgressBar.Color.NEGRO, ImageProgressBar.Color.NARANJA);
 		bProgreso.setImage(50, 10, BufferedImage.TYPE_4BYTE_ABGR, (float)37.5);
-//		if (bProgreso.getImageFile("imageProgressBarXXX.png")) {
-//			System.out.println("Imagen generada corréctamente.");
-//		}else {
-//			System.out.println("No se ha podido generar la imagen corréctamente.");
-//		}
+
 		var buffImage = bProgreso.getImg();
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		ImageIO.write(buffImage, "png", os);
 		InputStream is = new ByteArrayInputStream(os.toByteArray());
-//		return IOUtils.toByteArray(is);
-		var imagenTest = new ImgTest();
-		imagenTest.Image = IOUtils.toByteArray(is);
-		return ResponseEntity.ok(imagenTest);
-	}
-	
-	private class ImgTest{
-		public byte[] Image;
+
+		var array = IOUtils.toByteArray(is);
+		var imagen = new Image(array);
+		return ResponseEntity.ok(imagen);
 	}
 }
 
